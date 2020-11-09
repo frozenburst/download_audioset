4d2b4462217609738dbe915bda0092c102bf5d57
frozenburst <frozenburster@gmail.com>
2020-11-09 20:40:44 +0800

    Fix the bug of sortlist. And add data to gitignore.

--- as_download.py
+++ as_download.py
@@ -1,72 +1,72 @@
 '''
-================================================ 
-          DOWNLOAD_AUDIOSET REPOSITORY                     
-================================================ 
+================================================
+          DOWNLOAD_AUDIOSET REPOSITORY
+================================================
 
-repository name: download_audioset 
-repository version: 1.0 
-repository link: https://github.com/jim-schwoebel/download_audioset 
-author: Jim Schwoebel 
-author contact: js@neurolex.co 
-description: downloads the raw audio files from AudioSet (released by Google). 
-license category: opensource 
-license: Apache 2.0 license 
-organization name: NeuroLex Laboratories, Inc. 
-location: Seattle, WA 
-website: https://neurolex.ai 
-release date: 2018-11-08 
+repository name: download_audioset
+repository version: 1.0
+repository link: https://github.com/jim-schwoebel/download_audioset
+author: Jim Schwoebel
+author contact: js@neurolex.co
+description: downloads the raw audio files from AudioSet (released by Google).
+license category: opensource
+license: Apache 2.0 license
+organization name: NeuroLex Laboratories, Inc.
+location: Seattle, WA
+website: https://neurolex.ai
+release date: 2018-11-08
 
-This code (download_audioset) is hereby released under a Apache 2.0 license license. 
+This code (download_audioset) is hereby released under a Apache 2.0 license license.
 
-For more information, check out the license terms below. 
+For more information, check out the license terms below.
 
-================================================ 
-                SPECIAL NOTES                     
-================================================ 
+================================================
+                SPECIAL NOTES
+================================================
 
 This script parses through the entire balanced audioset dataset and downloads
 all the raw audio files. The files are arranged in folders according to their
 representative classes.
 
 Please ensure that you have roughly 35GB of free space on your computer before
-downloading the files. Note that it may take up to 2 days to fully download 
+downloading the files. Note that it may take up to 2 days to fully download
 all the files.
 
-Enjoy! - :) 
+Enjoy! - :)
 
--Jim 
+-Jim
 
-================================================ 
-                LICENSE TERMS                      
-================================================ 
+================================================
+                LICENSE TERMS
+================================================
 
-Copyright 2018 NeuroLex Laboratories, Inc. 
-Licensed under the Apache License, Version 2.0 (the "License"); 
-you may not use this file except in compliance with the License. 
-You may obtain a copy of the License at 
+Copyright 2018 NeuroLex Laboratories, Inc.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-     http://www.apache.org/licenses/LICENSE-2.0 
+     http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software 
-distributed under the License is distributed on an "AS IS" BASIS, 
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-See the License for the specific language governing permissions and 
-limitations under the License. 
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
-================================================ 
-                SERVICE STATEMENT                    
-================================================ 
+================================================
+                SERVICE STATEMENT
+================================================
 
-If you are using the code written for a larger project, we are 
-happy to consult with you and help you with deployment. Our team 
-has >10 world experts in Kafka distributed architectures, microservices 
-built on top of Node.js / Python / Docker, and applying machine learning to 
-model speech and text data. 
+If you are using the code written for a larger project, we are
+happy to consult with you and help you with deployment. Our team
+has >10 world experts in Kafka distributed architectures, microservices
+built on top of Node.js / Python / Docker, and applying machine learning to
+model speech and text data.
 
-We have helped a wide variety of enterprises - small businesses, 
-researchers, enterprises, and/or independent developers. 
+We have helped a wide variety of enterprises - small businesses,
+researchers, enterprises, and/or independent developers.
 
-If you would like to work with us let us know @ develop@neurolex.co. 
+If you would like to work with us let us know @ develop@neurolex.co.
 '''
 
 ################################################################################
@@ -75,17 +75,19 @@ If you would like to work with us let us know @ develop@neurolex.co.
 
 import pafy, os, shutil, time, ffmpy
 import pandas as pd
-import soundfile as sf 
+import soundfile as sf
 
 ################################################################################
 ##                            HELPER FUNCTIONS                                ##
 ################################################################################
 
-#function to clean labels 
+#function to clean labels
 def convertlabels(sortlist,labels,textlabels):
-    
+
     clabels=list()
-    
+    # Debug for sortlist data type, split with each label ids.
+    sortlist = sortlist.split(',')
+
     for i in range(len(sortlist)):
         #find index in list corresponding
         index=labels.index(sortlist[i])
@@ -93,7 +95,7 @@ def convertlabels(sortlist,labels,textlabels):
         #pull out converted label
         clabels.append(clabel)
 
-    return clabels 
+    return clabels
 
 def download_audio(link):
     listdir=os.listdir()
@@ -122,19 +124,19 @@ loadfile=pd.read_excel('labels.xlsx')
 number=loadfile.iloc[:,0].tolist()
 labels=loadfile.iloc[:,1].tolist()
 textlabels=loadfile.iloc[:,2].tolist()
-#remove spaces for folders 
+#remove spaces for folders
 for i in range(len(textlabels)):
     textlabels[i]=textlabels[i].replace(' ','')
 
 #now load data for youtube
-loadfile2=pd.read_excel('unbalanced_train_segments.xlsx')
+loadfile2=pd.read_excel('balanced_train_segments.xlsx')
 
-# ylabels have to be cleaned to make a good list (CSV --> LIST) 
+# ylabels have to be cleaned to make a good list (CSV --> LIST)
 yid=loadfile2.iloc[:,0].tolist()[2:]
 ystart=loadfile2.iloc[:,1].tolist()[2:]
 yend=loadfile2.iloc[:,2].tolist()[2:]
 ylabels=loadfile2.iloc[:,3].tolist()[2:]
- 
+
 #make folders
 try:
     defaultdir2=os.getcwd()+'/audiosetdata/'
@@ -148,8 +150,8 @@ for i in range(len(textlabels)):
     try:
         os.mkdir(textlabels[i])
     except:
-        pass 
-        
+        pass
+
 #iterate through entire CSV file, look for '--' if found, find index, delete section, then go to next index
 slink='https://www.youtube.com/watch?v='
 
@@ -160,21 +162,21 @@ for i in range(len(yid)):
     clabels=convertlabels(ylabels[i],labels,textlabels)
 
     for j in range(len(clabels)):
-        
+
         #change to the right directory
         newdir=defaultdir2+clabels[j]+'/'
         os.chdir(newdir)
-        
+
         if j ==0:
-            
-            #if it is the first download, pursue this path to download video 
+
+            #if it is the first download, pursue this path to download video
             lastdir=os.getcwd()+'/'
-    
+
             try:
                 # use YouTube DL to download audio
                 filename=download_audio(link)
                 extension='.m4a'
-                #get file extension and convert to .wav for processing later 
+                #get file extension and convert to .wav for processing later
                 os.rename(filename,'%s_start_%s_end_%s%s'%(str(i),start,end,extension))
                 filename='%s_start_%s_end_%s%s'%(str(i),start,end,extension)
                 if extension not in ['.wav']:
@@ -186,7 +188,7 @@ for i in range(len(yid)):
                         )
                     ff.run()
                     os.remove(filename+extension)
-                
+
                 file=filename+'.wav'
                 data,samplerate=sf.read(file)
                 totalframes=len(data)
@@ -198,7 +200,7 @@ for i in range(len(yid)):
                 sf.write('snipped'+file, data[startframe:endframe], samplerate)
                 snippedfile='snipped'+file
                 os.remove(file)
-                
+
             except:
                 print('no urls')
 
@@ -211,7 +213,7 @@ for i in range(len(yid)):
             except:
                 print('error copying file')
 
-    #sleep 5 seconds to prevent IP from getting banned 
-    time.sleep(5) 
+    #sleep 5 seconds to prevent IP from getting banned
+    time.sleep(5)
 
 
